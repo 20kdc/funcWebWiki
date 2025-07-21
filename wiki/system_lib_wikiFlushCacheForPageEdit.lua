6bf9f1bebf4505dc40d23b7a2ec8fa5508d7e6d3
t20kdc <asdd2808@gmail.com>
2025-07-21 13:20:52 +0100

    Note down some room for future expansion, minor tweaks...

--- wiki/system_lib_wikiFlushCacheForPageEdit.lua
+++ wiki/system_lib_wikiFlushCacheForPageEdit.lua
@@ -1,6 +1,9 @@
 -- A page has just been edited or deleted; flush the link cache.
 return function (path)
 	wikiDelete("system/cache/link/" .. path .. ".json")
+	-- Notably, it would be nice if some sort of 'template activation marker' remained here.
+	-- Chances are, though, this would imply turning <system/lib/wikiTemplate> into an AST component.
+	-- And reworking the visitor. But _in theory,_ it'd be really nice.
 	for _, v in ipairs(wikiPathList("special/")) do
 		wikiDelete("system/cache/link/" .. v .. ".json")
 	end
