cdd05d72687b6521aaa46c5620a0bd28e266953a
t20kdc <asdd2808@gmail.com>
2025-07-21 14:26:09 +0100

    account for Git changing files that we don't know about

--- wiki/system_lib_wikiPageLinks.lua
+++ wiki/system_lib_wikiPageLinks.lua
@@ -2,20 +2,34 @@
 -- Beware: The path given must be resolved, or bad weird things may happen.
 return function (path)
 	if path:sub(1, 13) == "system/cache/" then
-		-- don't even consider this for a cache file
+		-- don't even consider cache files
 		return {}
 	end
 	local cachePath = "system/cache/link/" .. path .. ".json"
+	-- checksum to detect changes from Git/etc.
+	local content = Slurp(path)
+	local contentCheck = content or ""
+	contentCheck = tostring(#contentCheck) .. "|" .. tostring(Crc32(0, contentCheck))
+	-- check cache
 	local existing = Slurp(cachePath)
 	if existing then
-		return DecodeJson(existing)
+		local dat = DecodeJson(existing)
+		if dat._check == contentCheck then
+			-- hide from caller
+			dat._check = nil
+			return dat
+		end
+		-- something went wrong; purge relevant cache to try and fix it
+		wikiFlushCacheForPageEdit(path)
 	end
 	-- fast-path for missing pages
-	if not Slurp(path) then
+	if not content then
 		return {}
 	end
 	local rendered = WikiTemplate(path, wikiDefaultOpts)
-	local links = {}
+	local links = {
+		_check = contentCheck
+	}
 	wikiAST.visit(function (node)
 		if getmetatable(node) == WikiLink then
 			local resolved = wikiResolvePage(node.page)
@@ -23,5 +37,7 @@ return function (path)
 		end
 	end, rendered)
 	Barf(cachePath, EncodeJson(links))
+	-- hide from caller
+	links._check = nil
 	return links
 end
