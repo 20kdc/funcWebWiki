541ebb14dc1c6b2fdd1e985de18e89d1652152cc
t20kdc <asdd2808@gmail.com>
2025-07-24 17:39:17 +0100

    Clean up SSG to produce a few less files

--- wiki/system_trigger_genSiteTar.lua
+++ wiki/system_trigger_genSiteTar.lua
@@ -26,6 +26,19 @@ end
 
 SetHeader("Content-Type", "application/octet-stream")
 
+local textishMimeTypesThatNeedRaw = {
+	["text/javascript"] = true,
+	["text/css"] = true
+}
+local function extNeedsRaw(ext)
+	local mime = wikiExtToMime(ext) or "text/plain"
+	-- if it's not textish, the fallback renderer wants to embed it as some kind of asset, so it is automatically raw.
+	-- then we have a list above of cases where we know this kind of asset will get embedded in HTML somehow and it is textish.
+	-- all assets are referenced via <system/lib/WikiLink> which cleans up the actual URLs.
+	-- therefore, we have a complete site.
+	return (not wikiExtText(ext)) or textishMimeTypesThatNeedRaw[mime]
+end
+
 if false then
 	-- debug TAR write code
 	dumpTarFile("test", "abc123\n")
@@ -35,12 +48,17 @@ else
 	for _, v in ipairs(wikiPathList()) do
 		-- re-virtualize names to avoid needing to handle relative traversal for file:
 		local vAdjName = v:gsub("/", "_")
-		dumpTarFile(vAdjName, wikiRead(v))
-		local template = WikiTemplate("system/index/frame", { path = v })
-		local html = wikiAST.renderToString(template, { renderType = "renderHtml", disableErrorIsolation = true, staticSite = true, absoluteBase = "" })
-		dumpTarFile(vAdjName .. ".html", html)
-		if v == preferredIndex then
-			dumpTarFile("index.html", html)
+		local _, vExt = wikiResolvePage(v)
+		if extNeedsRaw(vExt) then
+			dumpTarFile(vAdjName, wikiRead(v))
+		end
+		if wikiEnumPageFilter(v, {getParam = GetParam}, true) then
+			local template = WikiTemplate("system/index/frame", { path = v })
+			local html = wikiAST.renderToString(template, { renderType = "renderHtml", disableErrorIsolation = true, staticSite = true, absoluteBase = "" })
+			dumpTarFile(vAdjName .. ".html", html)
+			if v == preferredIndex then
+				dumpTarFile("index.html", html)
+			end
 		end
 	end
 end
