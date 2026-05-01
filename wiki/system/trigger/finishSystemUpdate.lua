a87fbd71f3c936676f80cdc2a5628dfa27cae992
t20kdc <asdd2808@gmail.com>
2026-05-01 11:13:49 +0100

    newHashes.json might not exist soooo

--- wiki/system/trigger/finishSystemUpdate.lua
+++ wiki/system/trigger/finishSystemUpdate.lua
@@ -1,3 +1,3 @@
--- Copies <system/newHashes.json> to <system/hashes.json>.
+-- Copies `system/newHashes.json` (might not exist) to <system/hashes.json>.
 wikiWrite("system/hashes.json", assert(wikiRead("system/newHashes.json"), "system/newHashes.json must exist."))
 return true
